dd8cea4646ad70d60c6a5b5e605e9ed6da741147
Gvidon <nide@inbox.ru>
2010-02-19 19:37:00 +0300

    pentackle

--- urls.py
+++ urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
 	url(r'^/?$'    , form, name='contactus'),
 	url(r'^search/', include('search.urls')),
 	url(r'^shop/'  , include('candy.urls')),
+	url(r'^blog/'  , include('pentackle.urls')),
 	url(r'^'       , include('accounts.urls')),
 )
 
