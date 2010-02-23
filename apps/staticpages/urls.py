15f3782754c79e1b11d38ec27036da2330f6de91
Gvidon <nide@inbox.ru>
2010-02-24 00:06:39 +0300

    static pages

---
+++ apps/staticpages/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('django.views.generic.simple',
+	url(r'^how-to-order/$'    , 'direct_to_template', {'template': 'staticpages/order.html'}     , name='how-to-order'),
+	url(r'^individual-order/$', 'direct_to_template', {'template': 'staticpages/individual.html'}, name='individual-order'),
+	url(r'^shipping/$'        , 'direct_to_template', {'template': 'staticpages/shipping.html'}  , name='shipping'),
+	url(r'^payment/$'         , 'direct_to_template', {'template': 'staticpages/payment.html'}   , name='payment'),
+	url(r'^partner/$'         , 'direct_to_template', {'template': 'staticpages/partner.html'}   , name='partner'),
+	url(r'^rules/$'           , 'direct_to_template', {'template': 'staticpages/rules.html'}     , name='rules'),
+	url(r'^contacts/$'        , 'direct_to_template', {'template': 'staticpages/contacts.html'}  , name='contacts'),
+)
