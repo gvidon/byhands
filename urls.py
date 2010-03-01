80c90ff020a0668ec1b4700a6ba54b89ec7a5944
Елена Кантемирова <imali@imali.local>
2010-03-01 23:31:24 +0300

    static content, markup

--- urls.py
+++ urls.py
@@ -12,11 +12,11 @@ urlpatterns = patterns('',
 	
 	url(r'^search/', include('search.urls')),
 	url(r'^shop/'  , include('candy.urls')),
-	url(r'^blog/'  , include('pentackle.urls')),
 	
 	url(r'^(?P<type>(articles|products)?)/by\-tag/(?P<tag>[\w\-_\!\s]+)', search, name='by-tag'),
 	
 	url(r'^'       , include('accounts.urls')),
+	url(r'^'       , include('pentackle.urls')),
 	url(r'^'       , include('staticpages.urls')),
 )
 
