227af6fd159d180deba3c7c2bcfe890d8c3d1882
Gvidon <nide@inbox.ru>
2010-03-05 02:42:32 +0300

    contactus form

--- apps/contactus/views.py
+++ apps/contactus/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import smtplib
 
-from django.shortcuts import render_to_response
-from forms            import ContactUsForm
+from django.template.context import RequestContext
+from django.shortcuts        import render_to_response
+from forms                   import ContactUsForm
 
 #CONTACTUS MAIN FORM
 def form(request):
@@ -16,21 +17,21 @@ def form(request):
 			
 			server = smtplib.SMTP('localhost')
 			
-			server.sendmail('From: no-reply@byhands.ru', 'To: info.byhands@gmail.com',
+			server.sendmail('From: no-reply@byhands.ru', 'To: info@byhands.ru',
 				'Subject: еще один хэндмейдер\r\n'+
-				request.POST['name']+' из '+request.POST['city']+'\n\n'
+				request.POST['name'].encode('utf8')+' из '+request.POST['city'].encode('utf8')+'\n\n'
 				
 				'Контакты: '+(', ').join([
 					type+': '+request.POST[type] for type in ['icq', 'email', 'phone'] if request.POST[type]
-				])+'\n\n'+
+				]).encode('utf8')+'\n\n'+
 				
-				'Интересен тем, что'+'\n'+request.POST['craft']+'\n\n'+
+				'Интересен тем, что'+'\n'+request.POST['craft'].encode('utf8')+'\n\n'+
 				'Может на заказ!'*int(request.POST.has_key('is_customable') and 1 or 0)
 			)
 			
 			server.quit()
 			
-			return render_to_response('contactus/thankyou.html')
+			return render_to_response('contactus/thankyou.html', context_instance=RequestContext(request))
 	
-	return render_to_response('contactus/index.html', { 'form': form })
+	return render_to_response('contactus/index.html', { 'form': form }, context_instance=RequestContext(request))
 	
