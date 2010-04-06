da319b1db0cf1885afaa1bd97eaf8f425082fa9c
Елена Кантемирова <imali@imali.local>
2010-04-07 00:46:02 +0400

    register-faq page

--- apps/staticpages/urls.py
+++ apps/staticpages/urls.py
@@ -2,11 +2,12 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('django.views.generic.simple',
-	url(r'^how-to-order/?$'    , 'direct_to_template', {'template': 'staticpages/order.html'}     , name='how-to-order'),
-	url(r'^individual-order/?$', 'direct_to_template', {'template': 'staticpages/individual.html'}, name='individual-order'),
-	url(r'^shipping/?$'        , 'direct_to_template', {'template': 'staticpages/shipping.html'}  , name='shipping'),
-	url(r'^payment/?$'         , 'direct_to_template', {'template': 'staticpages/payment.html'}   , name='payment'),
-	url(r'^rules/?$'           , 'direct_to_template', {'template': 'staticpages/rules.html'}     , name='rules'),
-	url(r'^contacts/?$'        , 'direct_to_template', {'template': 'staticpages/contacts.html'}  , name='contacts'),
-	url(r'^authors/?$'         , 'direct_to_template', {'template': 'staticpages/authors.html'}   , name='authors'),
+	url(r'^how-to-order/?$'    , 'direct_to_template', {'template': 'staticpages/order.html'}       , name='how-to-order'),
+	url(r'^individual-order/?$', 'direct_to_template', {'template': 'staticpages/individual.html'}  , name='individual-order'),
+	url(r'^shipping/?$'        , 'direct_to_template', {'template': 'staticpages/shipping.html'}    , name='shipping'),
+	url(r'^payment/?$'         , 'direct_to_template', {'template': 'staticpages/payment.html'}     , name='payment'),
+	url(r'^rules/?$'           , 'direct_to_template', {'template': 'staticpages/rules.html'}       , name='rules'),
+	url(r'^contacts/?$'        , 'direct_to_template', {'template': 'staticpages/contacts.html'}    , name='contacts'),
+	url(r'^authors/?$'         , 'direct_to_template', {'template': 'staticpages/authors.html'}     , name='authors'),
+	url(r'^register-faq/?$'    , 'direct_to_template', {'template': 'staticpages/registration.html'}, name='register-faq'),
 )
